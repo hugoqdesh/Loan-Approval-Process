34c6158192795044af885e53b011ea8485e50c1f
hugoqdesh <gitjohan5@gmail.com>
2026-04-09 15:47:40 +0300

    init

---
+++ src/test/java/com/example/loanapprovalprocess/LoanApprovalProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanapprovalprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApprovalProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
